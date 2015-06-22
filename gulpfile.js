05217ccc6b7996b4fb1a13a9ec4791f7190bd37d
Jayin Ton <tonjayin@gmail.com>
2015-06-22 21:07:00 +0800

    make dev better

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,41 @@ var gulp = require('gulp')
 var minifyCss = require('gulp-minify-css')
 var uglify = require('gulp-uglify')
 
-DEST = 'vendor'
+DEST = 'vendor';
+console.log(process.env);
+var IS_DEV = process.env || false;
 
 gulp.task('minify-css', function() {
-  return gulp.src('src/*.css')
-    .pipe(minifyCss())
-    .pipe(gulp.dest(DEST));
+    if (IS_DEV) {
+        gulp.src('src/*.css')
+            .pipe(gulp.dest(DEST));
+    } else {
+        gulp.src('src/*.css')
+            .pipe(minifyCss())
+            .pipe(gulp.dest(DEST));
+    }
+
 });
 
 gulp.task('compress', function() {
-  return gulp.src('src/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest(DEST));
+    if (IS_DEV) {
+        gulp.src('src/*.js')
+            .pipe(gulp.dest(DEST));
+    } else {
+        gulp.src('src/*.js')
+            .pipe(uglify())
+            .pipe(gulp.dest(DEST));
+    }
+
 });
 
-gulp.task('watch', ['minify-css', 'compress'], function(){
-    gulp.watch('src/*',['minify-css', 'compress'])        
-})
+gulp.task('watch', ['minify-css', 'compress'], function() {
+    gulp.watch('src/*', ['minify-css', 'compress'])
+});
+
+gulp.task('watch-dev', function() {
+    IS_DEV = true;
+    gulp.watch('src/*', ['minify-css', 'compress'])
+});
 
 gulp.task('default', ['watch'])
