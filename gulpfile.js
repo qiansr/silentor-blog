dc2388b8be5cdd9e584bd475a5ac2956ca6d9c88
Jayin Ton <tonjayin@gmail.com>
2015-06-23 14:30:16 +0800

    把全部需要的核心文件都打包成一个文件 close  #12

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
-var gulp = require('gulp')
-var minifyCss = require('gulp-minify-css')
-var uglify = require('gulp-uglify')
+var gulp = require('gulp');
+var minifyCss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 
 DEST = 'vendor';
 var IS_DEV = process.env.DEV || false;
@@ -29,13 +30,30 @@ gulp.task('compress', function() {
 
 });
 
-gulp.task('watch', ['minify-css', 'compress'], function() {
-    gulp.watch('src/*', ['minify-css', 'compress'])
+gulp.task('concat-js',['compress'], function(){
+    gulp.src(['vendor/highlight/highlight.pack.js',
+              'vendor/jquery-1.11.1.min.js',
+              'vendor/marked-0.3.2.min.js',
+              'vendor/blog.js'])
+        .pipe(concat('core.js'))
+        .pipe(gulp.dest(DEST))
+});
+
+gulp.task('concat-css',['minify-css'], function(){
+    gulp.src(['vendor/highlight/styles/github.css',
+              'vendor/github-markdown.css',
+              'vendor/blog.css'])
+        .pipe(concat('core.css'))
+        .pipe(gulp.dest(DEST))
+});
+
+gulp.task('watch', ['concat-css', 'concat-js'], function() {
+    gulp.watch('src/*', ['concat-css', 'concat-js'])
 });
 
 gulp.task('watch-dev', function() {
     IS_DEV = true;
-    gulp.watch('src/*', ['minify-css', 'compress'])
+    gulp.watch('src/*', ['minify-css', 'concat-js'])
 });
 
 gulp.task('default', ['watch'])
